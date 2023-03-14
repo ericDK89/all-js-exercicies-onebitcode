fd8cdc24fe8a2663ce0c640b7fd06e0eeaadf30d
ericDK89 <ericthr42@gmail.com>
2023-03-14 18:23:51 -0300

    tic-tac-toe started win/draw condition

--- tic-tac-toe/script.js
+++ tic-tac-toe/script.js
@@ -1,14 +1,21 @@
 const allSquares = document.querySelectorAll(".square");
 let whoTime = 0;
 
+const winsConditions = ["1", "2", "3"];
+
+const xArr = [];
+const oArr = [];
+
 allSquares.forEach((key) => {
   key.addEventListener("click", () => {
     if (whoTime % 2 === 0 && key.childElementCount !== 1) {
       key.innerHTML = "<span>X<span>";
       whoTime++;
+      xArr.push(key.dataset.number);
     } else if (whoTime % 2 !== 0 && key.childElementCount !== 1) {
       key.innerHTML = "<span>O<span>";
       whoTime++;
+      oArr.push(key.dataset.number);
     }
   });
 });
