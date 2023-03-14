54f3155b505c9fd121293540af0c9d7f11eac91a
ericDK89 <ericthr42@gmail.com>
2023-03-14 18:42:41 -0300

    tic-tac-toe reset btn done

--- tic-tac-toe/script.js
+++ tic-tac-toe/script.js
@@ -1,7 +1,15 @@
 const allSquares = document.querySelectorAll(".square");
 let whoTime = 0;
 
+let player1 = prompt("Qual o nome do jogador 1?");
+let player2 = prompt("Qual o nome do jogador 2?");
+
 const winsConditions = ["1", "2", "3"];
+
+const playerName = document.getElementById("player-name");
+playerName.innerText = `Vez do jogador: ${player1}`;
+
+const resetBtn = document.getElementById("reset-btn");
 
 const xArr = [];
 const oArr = [];
@@ -12,10 +20,22 @@ allSquares.forEach((key) => {
       key.innerHTML = "<span>X<span>";
       whoTime++;
       xArr.push(key.dataset.number);
+      playerName.innerText = `Vez do jogador: ${player2}`;
+      console.log(xArr);
     } else if (whoTime % 2 !== 0 && key.childElementCount !== 1) {
       key.innerHTML = "<span>O<span>";
       whoTime++;
       oArr.push(key.dataset.number);
+      playerName.innerText = `Vez do jogador: ${player1}`;
     }
   });
+});
+
+resetBtn.addEventListener("click", () => {
+  allSquares.forEach((key) => {
+    key.innerHTML = "";
+  });
+
+  xArr.splice(0, xArr.length + 1);
+  oArr.splice(0, oArr.length + 1);
 });
