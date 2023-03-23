08d24f129e21cdeda3e412850b2fed820a6e902c
ericDK89 <ericthr42@gmail.com>
2023-03-23 15:11:05 -0300

    exercicio 18

---
+++ exercicio-18/Product.js
@@ -0,0 +1,32 @@
+class Product {
+  constructor(name, description, price) {
+    this.name = name;
+    this.description = description;
+    this.price = price;
+    this.inStock = 0;
+    this.addToStock = function (quantity) {
+      this.inStock += quantity;
+    };
+  }
+
+  calculateDiscount(amount) {
+    if (amount > 0 && amount < 100) {
+      const discount = (amount / 100) * this.price;
+      const finalPrice = this.price - discount;
+      console.log(finalPrice);
+    } else if (amount >= 100) {
+      console.log("Free");
+    } else {
+      console.log("No discount");
+    }
+  }
+}
+
+const newProduct = new Product("Copo", "Carregar líquidos", 10);
+
+newProduct.addToStock(5);
+
+newProduct.calculateDiscount(0);
+
+console.log(newProduct);
+console.log(newProduct.calculateDiscount(90));
---
+++ exercicio-18/User.js
@@ -0,0 +1,19 @@
+class User {
+  constructor(name, email, password) {
+    this.name = name;
+    this.email = email;
+    this.password = password;
+  }
+
+  login(email, password) {
+    if (email && password === this.email && this.password) {
+      console.log("Sucesso");
+    } else {
+      console.log("Login ou senha incorretos");
+    }
+  }
+}
+
+const newUser = new User("Eric", "email@.com", "senha");
+
+newUser.login("Eric", "email@.com");
